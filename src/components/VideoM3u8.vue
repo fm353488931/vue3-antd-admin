718ab836cb9717d0965fa70b6f77d3d3807d2df5
fm <fm353488931@vip.qq.com>
2022-11-03 10:48:34 +0800

    m3u8组件

---
+++ src/components/VideoM3u8.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="VideoM3u8" v-if="url">
+    <video
+      id="myVideo"
+      :key="new Date()"
+      class="video-js vjs-default-skin vjs-big-play-centered"
+      controls
+      preload="auto"
+      data-setup="{}"
+      muted
+    >
+      <source id="source" :src="url" type="application/x-mpegURL" />
+    </video>
+  </div>
+</template>
+
+<script setup>
+import Videojs from 'video.js'
+import { nextTick, onMounted, onUnmounted } from 'vue'
+//弹窗
+const url = ref('')
+const myVideo = ref(null)
+onMounted(() => {
+  if (url.value) {
+    myVideo.value.dispose()
+    url.value = ''
+  } // 如果有先销毁
+  window.$api.monitorDetail({ params: { id: item.id } }).then((res) => {
+    url.value = res.url
+    nextTick(() => {
+      myVideo.value = Videojs('myVideo', {
+        bigPlayButton: true,
+        textTrackDisplay: false,
+        posterImage: false,
+        errorDisplay: false,
+      })
+      myVideo.value.play() // 视频播放
+    })
+  })
+})
+onUnmounted(() => {
+  if (url.value) {
+    myVideo.value.dispose()
+    url.value = ''
+  } // 如果有先销毁
+})
+</script>
+
+<style scoped lang="less">
+.VideoM3u8 {
+  width: 100%;
+  height: 100%;
+}
+</style>
